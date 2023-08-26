ca32da6a89b708e91ea99584993a31c6adafa41d
Alex Gay <alexgay27753@gmail.com>
2023-08-26 11:56:59 +0800

    okidokie commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
+import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -10,26 +13,30 @@ const Navbar = () => {
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>ORDER NOW!</div>
-          <div className={styles.text}>999 999 999</div>
+          <div className={styles.text}>999 999</div>
         </div>
       </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
+          <Link href="/" passHref>
+            <li className={styles.listItem}>Homepage</li>
+          </Link>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
-          <Image src="/img/pizzalogo.png" alt="" width={160} height={69} />
+          <Image src="/img/companylogo.png" alt="" width={69} height={69} />
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src="/img/cart.png" alt="" width={30} height={30} />
-          <div className={styles.counter}>2</div>
+      <Link href="/cart" passHref>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt="" width={30} height={30} />
+            <div className={styles.counter}>{quantity}</div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };

40fb335926f58a32933bdb087731900806a7bafe
Alex Gay <alexgay27753@gmail.com>
2023-08-31 14:37:06 +0800

    mediaquery commit

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -6,7 +6,7 @@ const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
       <Link href={`/product/${pizza._id}`} passHref>
-        <Image src={pizza.img} alt="" width={450} height={350} />
+        <Image src={pizza.img} alt="" width={450} height={350} className={styles.image}/>
       </Link>
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza.prices[0]}</span>
